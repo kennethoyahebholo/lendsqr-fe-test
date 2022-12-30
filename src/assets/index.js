429cb341031ed6524ae1a9933fdfce08ad11ab7f
kennethoyahebholo <www.oyahebholokennethizosea@gmail.com>
2022-12-30 22:13:52 +0100

    feat: implement top navbar feature

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,17 @@
 import Logo from "./images/login_logo.png";
 import ClipArt from "./images/clip_art.png";
+import DashboardLogo from "./images/logo.png";
+import Avatar from "./images/avatar.png";
+import DropIcon from "./images/dropdown.png";
+import SearchIcon from "./images/search.png";
+import notIcon from "./images/notification.png";
 
 export {
   Logo,
-  ClipArt
+  ClipArt,
+  DashboardLogo,
+  Avatar,
+  DropIcon,
+  SearchIcon,
+  notIcon
 }
--- src/components/TopNav/index.tsx
+++ src/components/TopNav/index.tsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import { FunctionComponent } from 'react';
+import "./TopNav.scss"
+import { DashboardLogo, Avatar, DropIcon, SearchIcon, notIcon } from '../../assets/index';
 
-type Props = {}
+export interface ITopNav {
+  label?: string;
+  dropdownObj?: any;
+}
 
-const index = (props: Props) => {
+
+const TopNav: FunctionComponent<ITopNav> = ({ dropdownObj }) => {
   return (
-    <div>Top Navigation</div>
+    <div className="navbar-cover d-flex items-center">
+     <div className="navbar-cover-logo">
+      <img src={DashboardLogo} alt="" />
+     </div>
+     <div className="navbar-cover-search">
+      <input
+         className="navbar-cover-search-input"
+         placeholder="search for anything"
+         />
+         <div className="navbar-cover-search-absolute">
+          <img src={SearchIcon} alt="" />
+         </div>
+      </div>
+      <div className="navbar-cover-details d-flex">
+       <div className='navbar-cover-details-lists'>
+        {/* make a link */}
+        <small>Docs</small>
+       </div>
+       <div className="navbar-cover-details-icon">
+          <img src={notIcon} alt="" />
+        </div>
+       <div className="navbar-cover-details-avatar"> 
+        <img src={Avatar} alt="" />        
+       </div> 
+       <div className="navbar-cover-details-names">
+        <h6>Adedeji <img src={DropIcon} alt="" /></h6>        
+       </div>     
+      </div>
+    </div>
   )
 }
 
-export default index
+export default TopNav
