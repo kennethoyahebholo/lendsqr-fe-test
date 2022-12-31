864c1a9c64b2600cc3cf4b35da3c27eec6c36aff
kennethoyahebholo <www.oyahebholokennethizosea@gmail.com>
2022-12-31 13:30:51 +0100

    fix: replaced png with svg files in the top-nav

--- src/components/TopNav/index.tsx
+++ src/components/TopNav/index.tsx
@@ -1,6 +1,11 @@
 import { FunctionComponent } from 'react';
 import "./TopNav.scss"
-import { DashboardLogo, Avatar, DropIcon, SearchIcon, notIcon } from '../../assets/index';
+
+import { ReactComponent as DashboardLogo } from "../../assets/svgs/navlogo.svg";
+import { ReactComponent as Avatar } from "../../assets/svgs/avatarsvg.svg";
+import { ReactComponent as DropIcon } from "../../assets/svgs/dropIcon.svg";
+import { ReactComponent as SearchIcon } from "../../assets/svgs/searhIcon.svg";
+import { ReactComponent as NotIcon } from "../../assets/svgs/npIcon.svg";
 
 import MobileMenuIcon from '../../assets/svgs/MobileMenuIcon';
 
@@ -26,32 +31,32 @@ const TopNav: FunctionComponent<ITopNav> = ({ dropdownObj }) => {
         </span>
 
       </div>
-     <div className="navbar-cover-logo">
-      <img src={DashboardLogo} alt="" />
-     </div>
-     <div className="navbar-cover-search">
-      <input
-         className="navbar-cover-search-input"
-         placeholder="search for anything"
-         />
-         <div className="navbar-cover-search-absolute">
-          <img src={SearchIcon} alt="" />
-         </div>
+      <div className="navbar-cover-logo">
+        <DashboardLogo />
       </div>
-      <div className="navbar-cover-details d-flex">
-       <div className='navbar-cover-details-lists'>
-        {/* make a link */}
-        <small>Docs</small>
-       </div>
-       <div className="navbar-cover-details-icon">
-          <img src={notIcon} alt="" />
+      <div className="navbar-cover-search">
+        <input
+          className="navbar-cover-search-input"
+          placeholder="search for anything"
+          />
+          <div className="navbar-cover-search-absolute">
+            <SearchIcon />
+          </div>
         </div>
-       <div className="navbar-cover-details-avatar"> 
-        <img src={Avatar} alt="" />        
-       </div> 
-       <div className="navbar-cover-details-names">
-        <h6>Adedeji <img src={DropIcon} alt="" /></h6>        
-       </div>     
+      <div className="navbar-cover-details d-flex">
+        <div className='navbar-cover-details-lists'>
+          {/* make a link */}
+          <small>Docs</small>
+        </div>
+        <div className="navbar-cover-details-icon">
+            <NotIcon />
+          </div>
+        <div className="navbar-cover-details-avatar"> 
+          <Avatar />        
+        </div> 
+        <div className="navbar-cover-details-names">
+          <h6>Adedeji <DropIcon /></h6>        
+        </div>     
       </div>
     </div>
   )
